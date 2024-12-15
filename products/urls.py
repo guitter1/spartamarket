7a6e6f8c64ffdb1eed5965585cba89eb88407743
guitter1 <hispalis0610@gmail.com>
2024-12-15 17:23:25 +0900

    좋아요 기능 구현

--- products/urls.py
+++ products/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("<int:pk>/delete/", views.delete, name="delete"),
     path("<int:pk>/comments/", views.comment_create, name="comment_create"),
     path("<int:pk>/comments_delete/", views.comment_delete, name="comment_delete"),
+    path("<int:pk>/like/", views.like, name="like"),
 ]
