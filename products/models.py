9a5bc4be4147a9c904c9e50db67c2a268cc8a50f
guitter1 <hispalis0610@gmail.com>
2024-12-15 14:10:06 +0900

    기본적인 게시기능 구현

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from PIL import Image
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    image=models.ImageField(upload_to="images/", blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image:
+            image_path = self.image.path
+            with Image.open(image_path) as img:
+                max_width, max_height = 250, 250
+                img.thumbnail((max_width, max_height))
+                img.save(image_path, quality=85, optimize=True)
+
+    def __str__(self):
+        return self.title
--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-
+from . import views
 app_name="products"
 
 urlpatterns = [
+    path("", views.products, name="products"),
+    path("new/", views.new, name="new"),
+    path("<int:pk>/", views.product_detail, name="product_detail"),
+    path("<int:pk>/edit/", views.edit, name="edit"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
 ]
