e1d50d335c5a13b9d608294dd68d80c44f06679d
guitter1 <hispalis0610@gmail.com>
2024-12-17 14:45:40 +0900

    해시태그기능 추가

--- products/forms.py
+++ products/forms.py
@@ -1,14 +1,18 @@
 from django import forms
 
-from .models import Product, Comment
+from .models import Product, Comment, Hashtag
 
 class ProductForm(forms.ModelForm):
+    hashtags = forms.CharField(
+    required=False,
+    widget=forms.TextInput(attrs={'placeholder': '해시태그를 쉼표로 구분해서 입력하세요'})
+    )
     class Meta:
         model = Product
         fields = "__all__"
         exclude=('author', 'like_users', 'views')
 
-...
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -24,3 +28,8 @@ class OrderForm(forms.Form):
     ]
 
     category = forms.ChoiceField(choices=CATEGORY_CHOICES, initial='pk', required=False)
+
+class HashtagForm(forms.ModelForm):
+    class Meta:
+        model=Hashtag
+        fields = "__all__"
